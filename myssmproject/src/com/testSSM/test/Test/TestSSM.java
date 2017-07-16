44756cf0afba9902157cd0ee3f844265a72292e9
xmh <15201131872@163.com>
2017-07-16 17:16:06 +0800

    commit

---
+++ myssmproject/src/com/testSSM/test/Test/TestSSM.java
@@ -0,0 +1,18 @@
+package com.testSSM.test.Test;
+
+import org.apache.log4j.chainsaw.Main;
+
+import com.testSSM.test.model.User;
+import com.testSSM.test.service.TestServiceImpl;
+
+public class TestSSM {
+
+	public static void main(String[] args){
+		TestServiceImpl  testimpl=new TestServiceImpl();
+
+		
+		System.out.println("this is hh");
+
+		
+	}
+}
