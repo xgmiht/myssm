6955fbef0d9e031dc84acdf38642e545e38bc8aa
xmh <15201131872@163.com>
2017-07-16 19:49:31 +0800

    ffff

--- myssmproject/src/com/testSSM/test/Test/TestSSM.java
+++ myssmproject/src/com/testSSM/test/Test/TestSSM.java
@@ -10,5 +10,6 @@ public class TestSSM {
 	public static void main(String[] args){
 		TestServiceImpl  testimpl=new TestServiceImpl();
 System.out.println("this is hh");
+System.out.println();
 	}
 }
